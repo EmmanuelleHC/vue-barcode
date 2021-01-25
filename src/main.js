7863316d982470d0ae7c89eb1c011088f082eada
EmmanuelleHC <ehcwijaya@gmail.com>
2021-01-25 22:47:31 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueBarcode from '@chenfengyuan/vue-barcode';
+import vuetify from './plugins/vuetify';
 
+Vue.component(VueBarcode.name, VueBarcode);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
